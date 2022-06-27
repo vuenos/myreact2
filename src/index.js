06341b14d128ed2fed4746bb6b97b186bf99c6a9
JintaeKim <vuenos@naver.com>
2022-06-27 22:58:54 +0900

    Ready to React_Query

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClient, QueryClientProvider } from 'react-query';
 import reportWebVitals from './reportWebVitals';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-ReactDOM.render(
-      <App />,
-  document.getElementById('root')
+const QueryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
