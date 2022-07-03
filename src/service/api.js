103aa1b07430127c11aff33794475a8870c64e16
Jintae Kim <vuenos@gmail.com>
2022-07-03 17:36:39 +0900

    TMDM Api network test

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: '',
+    baseURL: process.env.REACT_APP_PROXY,
     withCredential: true,
     headers: {
         'Accept': 'application/json',
