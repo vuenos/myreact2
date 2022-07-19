6e991c3a75a9954f2f70bb4f86753d1b282ec6e0
JintaeKim <vuenos@naver.com>
2022-07-19 23:59:53 +0900

    Routes set

--- src/App.js
+++ src/App.js
@@ -1,68 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import apiClient from './service/api';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home, Movies } from './pages';
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [movies, setMovies] = useState([]);
-  const [pages, setPages] = useState("");
-
-  const getMovies = async () => {
-    try {
-      const {data, status} = await apiClient.get(`/popular?api_key=${process.env.REACT_APP_APIKEY}&page=1`);
-      if(status === 200) {
-
-        setLoading(true);
-        setTimeout(()=> {
-          setLoading(false);
-          setMovies(data.results);
-          setPages(data);
-        }, 1000)
-        console.log('MOVIE_DATA', data.results)
-      }
-
-    } catch(error) {
-      //
-      console.log(error.message);
-    }
-  }
-
-  useEffect(()=> {
-    getMovies();
-  }, [])
 
   return (
     <div>
-      <h1>App</h1>
-      {loading && <div>Loading....</div>}
-
-      <div>
-        {pages.total_pages}
-        {pages.total_pages > 1 && (
-          <div>
-            {[...Array(pages.total_pages).keys()].map((x) => (
-              <Link
-                key={x + 1}
-                to={`x + 1`}
-              >
-                <span>{x + 1}</span>
-              </Link>
-            )).slice(0, 10)}
-          </div>
-        )}
-      </div>
-
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <p><img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} width="240" /></p>
-            <h2><a href={``}>{movie.original_title}</a></h2>
-            <div>
-              {movie.overview}
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+
+import Home from "./Home";
+import Movies from "./Movies";
+
+export { Home, Movies }
