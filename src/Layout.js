cecee1ec66c32556dc71332fcb6fdb516cc38867
Jintae Kim <vuenos@naver.com>
2022-07-24 17:41:48 +0900

    Layout add Container, Box from MUI

--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header, Footer } from './components';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const Layout = () => {
   return (
     <div>
         <Header />
         <main>
-            <Outlet />
+            <CssBaseline />
+            <Container maxWidth="xl">
+                <Box sx={{ height: '100%' }}>
+                    <Outlet />
+                </Box>
+            </Container>
         </main>
         <Footer />
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import FolderIcon from '@mui/icons-material/Folder';
+import Paper from '@mui/material/Paper';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -14,24 +15,27 @@ const Footer = () => {
   };
 
   return (
-    <BottomNavigation sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-      />
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation showLabels value={value} onChange={handleChange}>
+        <BottomNavigationAction
+          label="Recents"
+          value="recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOnIcon />}
+        />
+        <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+      </BottomNavigation>
+    </Paper>
+    
   )
 }
 
