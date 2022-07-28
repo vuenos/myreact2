9735da42b364ba1015317a7cee7247a5983e2aa1
Jintae kim <vuenos@naver.com>
2022-07-28 18:13:52 +0900

    writing Genre components

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -10,6 +10,24 @@ const Genres = ({
     type,
     setPage
 }) => {
+
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genre.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+
+
+
   return (
     <div>Genres</div>
   )
