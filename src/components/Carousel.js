b5fad14afedfa8ec732e56e175cca457c28c22ac
Jintae Kim <vuenos@naver.com>
2022-07-30 16:00:03 +0900

    add Carousel Components

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import apiClient from '../service/api';
 import { img_300, noPicture } from '../config/config';
 import "./Carousel.css";
 
-const Carousel = () => {
+const handleDragStart = (e) => e.preventDefault();
+
+const Carousel = ({ id, media_type }) => {
+  const [credits, setCredits] = useState([]);
+
+  const items = credits.map((c) => (
+    <div className='carouselItem'>
+      <img 
+        src={c.profile_path ? `${img_300}/${c.profile_path}` : noPicture}
+        alt={c?.name}
+        onDragStart={handleDragStart}
+        className="carouselItem_img"
+      />
+      <b className="carouselItem_txt">{c?.name}</b>
+    </div>
+  ));
+
+  const responsive = {
+    0: {
+      items: 3,
+    },
+    512: {
+      items: 5,
+    },
+    1024: {
+      items: 7,
+    }
+  };
+
+  const getCredits = async () => {
+    const { data } = await apiClient.get(`/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`);
+
+    setCredits(data.cast);
+  }
+
+  useEffect(() => {
+    getCredits();
+  }, [])
+
   return (
-    <div>Carousel</div>
+    <divAliceCarousel 
+      mouseTracking
+      infinite
+      disableDotsControls
+      disableButtonsControls
+      responsive={responsive}
+      items={items}
+      autoPlay
+    />
   )
 }
 
