00f5c1feea41fafea00c63e324c1b7e109e36ed2
Jintae Kim <vuenos@naver.com>
2022-08-03 22:43:21 +0900

    Add to title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import apiClient from '../service/api';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 const Home =()=> {
     return (
-        <h1>Home</h1>
+        <Box sx={{ width: '100%', maxWidth: 1280 }}>
+           <Typography variant="h2" gutterBottom component="h2">
+                Movies
+            </Typography>
+
+            <Typography variant="h2" gutterBottom component="h2">
+                Populars
+            </Typography>
+        </Box>
+        
     );
 }
 
