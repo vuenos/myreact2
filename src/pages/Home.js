3811c25a818b07b6142fa5076657b3eb434daa73
Jintae Kim <vuenos@naver.com>
2022-08-10 22:59:13 +0900

    Display movies to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import apiClient from '../service/api';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { useQuery } from 'react-query';
 
 
-const Home =()=> {
-    const { isLoading, error, data } = useQuery(['repoData'], () => fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res => res.json()
-        )
-    )
+const Home =({ media_type, id })=> {
+  const [movie, setMovie] = useState([])
+    
+    const getVideo = async () => {
+      const { data } = await apiClient.get(`/${media_type}/${id}?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`);
+  
+      try {
+        setMovie(data.results);
+      } catch(err) {
+        //
+      }
+    };
 
-    if (isLoading) return 'Loading...'
-    if (error) return 'An error has occurred:' + error.message
+    useEffect(() => {
+      setMovie();
+    }, []);
+    
 
     return (
         <Box sx={{ width: '100%', maxWidth: 1280 }}>
            <Typography variant="h2" gutterBottom component="h2">
-                {data.name}
-            </Typography>
-            <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{' '}
-      <strong>✨ {data.stargazers_count}</strong>{' '}
-      <strong>🍴 {data.forks_count}</strong>
-
-            <Typography variant="h2" gutterBottom component="h2">
-                Populars
+                {movie.name}
             </Typography>
         </Box>
         
