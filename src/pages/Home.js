86666dd8c5b8353ba24151e63ffe73f211eb210b
Jintae Kim <vuenos@naver.com>
2022-08-12 23:29:23 +0900

    Movie Card insert to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import apiClient from '../service/api';
+import {
+  img_500,
+  unavailable
+} from '../config/config';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 import Box from '@mui/material/Box';
 
 
@@ -24,11 +32,34 @@ const Home =()=> {
 
     return (
         <Box sx={{ width: '100%', maxWidth: 1280 }}>
-           {movies.map((movie) => (
-            <Typography variant="h2" gutterBottom component="h2" key={movie.id}>
-              {movie.title}
-            </Typography>
-           ))}
+          {movies.map((movie) => (
+           <Card sx={{ maxWidth: 345 }} key={movie.id}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={movie.poster_path
+                  ? `${img_500}/${movie.poster_path}`
+                  : unavailable}
+                alt={movie.title}
+               />
+              <CardContent>
+              
+                <Typography gutterBottom variant="h5" component="h5">
+                  {movie.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {movie.overview}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Share
+              </Button>
+            </CardActions>
+           </Card>
+          ))}
         </Box>
         
     );
